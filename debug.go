cb9ffd4499f53052051d02df404bb29221ff563b
Matt Cornell <matt@teamcornell.com>
2018-11-23 12:08:56 -0800

    first commit

---
+++ debug.go
@@ -0,0 +1,44 @@
+package vera 
+
+import ( 
+"fmt"
+"io"
+"io/ioutil"
+"time"
+)
+
+var DebugOpt bool
+var DOut io.Writer = ioutil.Discard
+var	DebugTime   = time.Now()
+
+func DPause(this string) {
+	if DebugOpt {
+		fmt.Fprintf(DOut, "%v", this)
+		var answer string
+		fmt.Scanf("%s", &answer)
+	}
+}
+
+func DWhatsThis(this interface{}) {
+	if DebugOpt {
+		fmt.Fprintf(DOut, "%T: %v\n", this, this)
+	}
+}
+
+func DTook(t time.Time, msg string) {
+    if DebugOpt {
+        fmt.Fprintf(DOut, "took: %v: %v\n", msg, time.Since(t))
+    }
+}
+
+func DTime(msg string) {
+	if DebugOpt {
+		fmt.Fprintf(DOut, "time: %v		 %v", time.Since(DebugTime), msg)
+	}
+}
+func DMsg(msg string) {
+	if DebugOpt {
+		fmt.Fprintf(DOut, "%v", msg)
+	}
+}
+
